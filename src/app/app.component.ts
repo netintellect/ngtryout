3f64ccc163dadefc77d49756698f1c37509a7742
Patrick Wolfs <patrick.wolfs@netintellect.be>
2017-08-02 16:25:01 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div>
+      <h1>{{header}}</h1>
+      <ul>
+        <li *ngFor="let line of lines">
+        {{line}}
+        </li>
+      </ul>
+      <form>
+        <div>
+          <label for="new-color-input">New Color:</label>
+          <input type="text" 
+                 id="new-color-input" 
+                 name="newColor"
+                [(ngModel)]="newColor">
+          <button type="button" (click)="addColor($)">Add color</button>
+        </div>
+      </form>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public header: string = "app with new header";
+  public lines : string[] = [
+    "red", "white", "blue"
+  ];
+  public newColor: string = "";
+
+  public addColor(): void {
+    this.lines.push(this.newColor);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from "@angular/forms";
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import '../styles/site.css';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
